d48280cd5a032382325517157108c6426593ed97
Tomáš Tokoš <tokos@comto.sk>
2022-01-16 15:34:02 +0100

    dalsia verzia s post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone   # pridane
+from .models import Post            # na pridanie modelu z models.py
 
 # Create your views here.
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
