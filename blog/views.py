e0f24866bdc1211978a3ebe6dc7db9bbf88fc04a
Tomáš Tokoš <tokos@comto.sk>
2022-01-16 18:55:20 +0100

    robime Šablónové tagy v Djangu ktor=e umožňujú previesť Pythonovské veci do HTML, +c=im dynamické webové stránky môžno vytvoriť rýchlejšie. V predchádzajúcej kapitole sme našej šablóne dali zoznam príspevkov v premennej posts. Teraz to zobrazíme v HTML. Použijeme dvojitú zloženú zátvorku s názvom premennej vo vnútri {{ posts}}

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from django.utils import timezone   # pridane
 from .models import Post            # na pridanie modelu z models.py
 
 # Create your views here.
-def post_list(request):
+def post_list(request):    # vytvorili sme premennu post na odovzdavanie QuerySet ktora je aj jeho menom
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+                            # Posledný parameter, {} je miesto na ktoré môžeme dať nejaké veci
+                            # ktoré môže použiť šablóna. Potrebujeme im dať mená (zatiaľ sa
+                            # budeme držať 'posts'). :) Malo by to vyzerať takto: {'posts': posts}.
+                            # Všimni si, že časť pred : je reťazec: potrebuješ ju obaliť do úvodzoviek:"
