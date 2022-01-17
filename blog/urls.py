3885a01df4df2ae4fc26b267e9b02c6783b93c74
Tomáš Tokoš <tokos@comto.sk>
2022-01-17 10:18:06 +0100

    Rozsirovanie aplikacie - pridanie post_detail do nasho view a osetrenie situacie ak nie je ziaden Post s danym pk

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # r'^$' nahradene '', post_list je home page
-]
         # po ^ (začiatku) bude nasledovať $ (koniec)
         # Tento vzor povie Djangu, že views.post_list je to správne miesto,
         # kam treba ísť, ak niekto vstúpi na stránku cez adresu 'http://127.0.0.1:8000/'
         # name='post_list' je názov URL, ktorý sa použije na identifikáciu zobrazenia
+    path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+        # odkazanie Django na view nazvaný post_detail, ktorý zobrazí celý príspevok blogu)
+]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone   # pridane
 from .models import Post            # na pridanie modelu z models.py
-
+from django.shortcuts import render, get_object_or_404
+                                    # na pridanie modelu z models.py
 # Create your views here.
 def post_list(request):    # vytvorili sme premennu post na odovzdavanie QuerySet ktora je aj jeho menom
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +11,8 @@ def post_list(request):    # vytvorili sme premennu post na odovzdavanie QuerySe
                             # ktoré môže použiť šablóna. Potrebujeme im dať mená (zatiaľ sa
                             # budeme držať 'posts'). :) Malo by to vyzerať takto: {'posts': posts}.
                             # Všimni si, že časť pred : je reťazec: potrebuješ ju obaliť do úvodzoviek:"
+
+def post_detail(request, pk):   # pridanie nasho view pre post_detail
+    post = get_object_or_404(Post, pk=pk)   # osetrenie situacie ak nie je ziaden Post s danym pk
+                                # zobrazí sa stránka, Page Not Found 404, ktoru mozno upravit
+    return render(request, 'blog/post_detail.html', {'post': post})
